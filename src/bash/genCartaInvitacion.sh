dfe43195c44ba3fb94ac0c7838e13de15eb52316
dmallcott <daniel.mallcott@gmail.com>
2013-12-29 14:44:33 -0430

    En este commit modifique el documento final generado de forma tal que sea un pdf y no un odt.

--- src/bash/genCartaInvitacion.sh
+++ src/bash/genCartaInvitacion.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Se comienza por extraer los componentes del documento
 unzip ./src/documents/templates/carta_invitacion.odt -d $1
 
+# Luego se modifica el archivo content.xml para cambiar las 
+# etiquetas previamente definidas por los valores pasados 
+# como argumentos
 cd $1
 sed -i 's/.%CODIGO%./>'$1'</g' content.xml
 sed -i 's/.%FECHA%./>'$2'</g' content.xml
@@ -16,8 +20,15 @@ sed -i 's/.%CONTACTO%./ '"${10}"'</g' content.xml
 sed -i 's/.%RESPONSABLE%./>'"${11}"'</g' content.xml
 sed -i 's/.%UNIDAD_SOLICITANTE%./>'"${12}"'</g' content.xml
 
+# Seguido de comprimir de nuevo el documento con los cambios hechos,
+# moverlo a la carpeta de documentos generados y 
+# eliminar el directorio donde se descomprimio
 zip -r carta_invitacion_$1.odt *
 cp carta_invitacion_$1.odt ../src/documents/generated
 cd ..
 rm -rf $1/
+rm *.sh~
 
+# Por ultimo se usa unoconv para generar el .pdf y luego se elimina el .odt
+unoconv -f pdf src/documents/generated/carta_invitacion_$1.odt
+rm src/documents/generated/carta_invitacion_$1.odt
