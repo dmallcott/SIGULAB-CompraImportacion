78c5013e24d171a1b5f48eb82df91f8ac8d4472b
Daniel Malcott <dmalcott@gmail.com>
2014-01-23 09:24:06 -0430

    no se que hace este.

---
+++ src/java/Actions/Documentos/CartaInvitacion/ReqEditarCartaInvitacion.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Actions.Documentos.CartaInvitacion;
+
+import Clases.CartaInvitacion;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+/**
+ *
+ * @author daniel
+ */
+public class ReqEditarCartaInvitacion extends org.apache.struts.action.Action {
+    
+    private static final String SUCCESS = "success";
+    
+     /**
+     * This is the action called from the Struts framework.
+     *
+     * @param mapping The ActionMapping used to select this instance.
+     * @param form The optional ActionForm bean for this request.
+     * @param request The HTTP Request we are processing.
+     * @param response The HTTP Response we are processing.
+     * @throws java.lang.Exception
+     * @return
+     */
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception 
+    {
+        HttpSession session = request.getSession(true); 
+        CartaInvitacion carta = (CartaInvitacion) form;
+        String codCarta = (String) request.getParameter("codCartaInvitacion");
+        carta = DBMS.DBMS.getInstance().getCartaInvitacion(codCarta);
+        request.setAttribute("CartaInvitacion", carta);
+        return mapping.findForward(SUCCESS);
+    }
+}
